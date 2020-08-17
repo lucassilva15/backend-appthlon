1cbee7184ec9d9e972127dddeb34e1c573c29124
Lucas Silva <lucas.silva15@universo.univates.br>
2020-08-17 20:23:29 -0300

    Add config to heroku

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from './app';
 
-app.listen(3333, () => console.log(`App listening in localhost:3333`));
+app.listen(process.env.PORT || 3333, () =>
+  console.log(`App listening in localhost:3333`)
+);
